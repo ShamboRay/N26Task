718c003421a734a108344a560e4c22d67c792a12
Ray <sray8@corp.intuit.net>
2019-04-08 02:27:08 +0530

    Delete unneccessary files

--- force-app/main/default/aura/Towermap/TowermapController.js
+++
@@ -1,8 +0,0 @@
-({
-     handleInit: function (component, event, helper) {
-          helper.initHelper(component, event, helper);
-     },
-    handleClick: function (component, event, helper) {
-          alert('In the fn');
-     }
-})
